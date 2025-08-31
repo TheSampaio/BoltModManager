0982e4c19484804460b4d269a833564d069844e0
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-31 18:59:16 -0300

    Add SymbolicLink class and its flags (Enum)

---
+++ Bolt/Enums/SymbolicLinkFlags.cs
@@ -0,0 +1,8 @@
+﻿namespace Bolt.Enums
+{
+    internal enum SymbolicLinkFlags
+    {
+        DirectoryMode = 0x1,
+        FileMode = 0x0,
+    }
+}
---
+++ Bolt/Utilities/SymbolicLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Bolt.Utilities
+{
+    internal class SymbolicLink
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        static extern bool Create(string symlinkFileName, string targetFileName, int flags);
+    }
+}
