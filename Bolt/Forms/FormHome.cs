53e17a81d16bdd31c8618cf240a35edc686b3eca
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-31 17:13:42 -0300

    Add form new game as modal window

--- Bolt/Forms/FormHome.cs
+++ Bolt/Forms/FormHome.cs
@@ -1,10 +1,16 @@
 ﻿namespace Bolt.Forms
 {
-    public partial class FormHome : Form
+    public partial class FrmHome : Form
     {
-        public FormHome()
+        public FrmHome()
         {
             InitializeComponent();
+        }
+
+        private void NewGame_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frmNewGame = new FrmNewGame();
+            frmNewGame.ShowDialog();
         }
 
         private void Quit_ToolStripMenuItem_Click(object sender, EventArgs e)
