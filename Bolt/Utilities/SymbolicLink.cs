52eb16494d6fd4f784226d7b335d363b3572e183
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-31 19:33:31 -0300

    Update SymbolicLink.cs with a better abstraction

--- Bolt/Utilities/SymbolicLink.cs
+++ Bolt/Utilities/SymbolicLink.cs
@@ -1,10 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using Bolt.Enums;
+using System.Runtime.InteropServices;
 
 namespace Bolt.Utilities
 {
     internal class SymbolicLink
     {
+        public static bool Create(string symlinkFileName, string targetFileName, SymbolicLinkType type) =>
+            CreateSymbolicLink(symlinkFileName, targetFileName, (int)type);
+
+#pragma warning disable IDE0079 // Remove unnecessary suppression
+#pragma warning disable SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        public static extern bool Create(string symlinkFileName, string targetFileName, int flags);
+        static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+
+#pragma warning restore SYSLIB1054
+#pragma warning restore IDE0079
     }
 }
