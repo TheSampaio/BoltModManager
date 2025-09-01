a03176f257db78cff68be9e34139b508f1b6721a
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-09-01 08:54:11 -0300

    Update Json class to support multiple contexts

--- Bolt/Data/AppDbContext.cs
+++ Bolt/Data/AppDbContext.cs
@@ -4,11 +4,15 @@ namespace Bolt.Data
 {
     internal class AppDbContext
     {
+        // Contexts
+        public static string SettingsFile { get; } = "settings.json";
+
+        // Keys and values
         public static string ModsDirectoryKey { get; } = "ModsDirectory";
         public static string ModsDirectoryValue
         {
-            get => Json.Read(ModsDirectoryKey)!;
-            set => Json.Write(ModsDirectoryKey, value);
+            get => Json.Read(ModsDirectoryKey, SettingsFile)!;
+            set => Json.Write(ModsDirectoryKey, value, SettingsFile);
         }
     }
 }
