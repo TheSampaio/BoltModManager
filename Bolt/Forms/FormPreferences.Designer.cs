71d27912bf9fe58eb5340c3191f3112a4ba33aa7
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-31 21:49:50 -0300

    Add save mods directory feature

--- Bolt/Forms/FormPreferences.Designer.cs
+++ Bolt/Forms/FormPreferences.Designer.cs
@@ -30,8 +30,8 @@
         {
             panel1 = new Panel();
             BtnSave = new Button();
-            BtnCancel = new Button();
-            TxyLibrary = new Bolt.Controls.TextEntry();
+            BtnClose = new Button();
+            TxyModsDir = new Bolt.Controls.TextEntry();
             BtnLibrary = new Button();
             FbdLibrary = new FolderBrowserDialog();
             SuspendLayout();
@@ -43,47 +43,48 @@
             panel1.Location = new Point(12, 246);
             panel1.Name = "panel1";
             panel1.Size = new Size(360, 1);
-            panel1.TabIndex = 6;
+            panel1.TabIndex = 0;
             // 
             // BtnSave
             // 
             BtnSave.Location = new Point(216, 258);
             BtnSave.Name = "BtnSave";
             BtnSave.Size = new Size(75, 23);
-            BtnSave.TabIndex = 7;
+            BtnSave.TabIndex = 2;
             BtnSave.Text = "Save";
             BtnSave.UseVisualStyleBackColor = true;
             BtnSave.Click += BtnSave_Click;
             // 
-            // BtnCancel
+            // BtnClose
             // 
-            BtnCancel.Location = new Point(297, 258);
-            BtnCancel.Name = "BtnCancel";
-            BtnCancel.Size = new Size(75, 23);
-            BtnCancel.TabIndex = 8;
-            BtnCancel.Text = "Cancel";
-            BtnCancel.UseVisualStyleBackColor = true;
-            BtnCancel.Click += BtnCancel_Click;
+            BtnClose.Location = new Point(297, 258);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new Size(75, 23);
+            BtnClose.TabIndex = 3;
+            BtnClose.Text = "Close";
+            BtnClose.UseVisualStyleBackColor = true;
+            BtnClose.Click += BtnClose_Click;
             // 
-            // TxyLibrary
+            // TxyModsDir
             // 
-            TxyLibrary.Location = new Point(17, 28);
-            TxyLibrary.Margin = new Padding(8, 8, 4, 8);
-            TxyLibrary.Name = "TxyLibrary";
-            TxyLibrary.ReadOnly = false;
-            TxyLibrary.Size = new Size(319, 44);
-            TxyLibrary.TabIndex = 9;
-            TxyLibrary.Text = "Games directory:";
+            TxyModsDir.Location = new Point(17, 28);
+            TxyModsDir.Margin = new Padding(8, 8, 4, 8);
+            TxyModsDir.Name = "TxyModsDir";
+            TxyModsDir.ReadOnly = false;
+            TxyModsDir.Size = new Size(319, 44);
+            TxyModsDir.TabIndex = 0;
+            TxyModsDir.Text = "Mods directory:";
+            TxyModsDir.Value = "";
             // 
             // BtnLibrary
             // 
             BtnLibrary.Location = new Point(343, 48);
             BtnLibrary.Name = "BtnLibrary";
             BtnLibrary.Size = new Size(24, 24);
-            BtnLibrary.TabIndex = 10;
+            BtnLibrary.TabIndex = 1;
             BtnLibrary.Text = "...";
             BtnLibrary.UseVisualStyleBackColor = true;
-            BtnLibrary.Click += BtnLibrary_Click;
+            BtnLibrary.Click += BtnModsDir_Click;
             // 
             // FrmPreferences
             // 
@@ -91,11 +92,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             ClientSize = new Size(384, 293);
-            Controls.Add(TxyLibrary);
+            Controls.Add(TxyModsDir);
             Controls.Add(BtnLibrary);
             Controls.Add(panel1);
             Controls.Add(BtnSave);
-            Controls.Add(BtnCancel);
+            Controls.Add(BtnClose);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -103,6 +104,7 @@
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Preferences";
+            Load += FrmPreferences_Load;
             ResumeLayout(false);
         }
 
@@ -110,8 +112,8 @@
 
         private Panel panel1;
         private Button BtnSave;
-        private Button BtnCancel;
-        private Controls.TextEntry TxyLibrary;
+        private Button BtnClose;
+        private Controls.TextEntry TxyModsDir;
         private Button BtnLibrary;
         private FolderBrowserDialog FbdLibrary;
     }
--- Bolt/Forms/FormPreferences.cs
+++ Bolt/Forms/FormPreferences.cs
@@ -1,29 +1,58 @@
-﻿namespace Bolt.Forms
+﻿using Bolt.Utilities;
+
+namespace Bolt.Forms
 {
     public partial class FrmPreferences : Form
     {
+        private const string MODS_DIR = "ModsDirectory";
+
         public FrmPreferences()
         {
             InitializeComponent();
         }
 
-        private void BtnLibrary_Click(object sender, EventArgs e)
+        private void BtnModsDir_Click(object sender, EventArgs e)
         {
             FbdLibrary.ShowDialog(this);
 
             if (FbdLibrary.SelectedPath != string.Empty)
-                TxyLibrary.Value = FbdLibrary.SelectedPath;
+                TxyModsDir.Value = FbdLibrary.SelectedPath;
         }
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
-            // TODO: Validade entries
-            // TODO: Save button logic...
+            // Validade entry
+            if (TxyModsDir.Value == string.Empty)
+            {
+                MessageBox.Show(
+                    "The mods directory cannot be empty.",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+            // Save mods directory path
+            Json.Write(MODS_DIR, TxyModsDir.Value);
+
+            MessageBox.Show(
+                $"The directory \"{TxyModsDir.Value}\" has been successfully saved as the mods folder.",
+                "Mods Directory Saved",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
         }
 
-        private void BtnCancel_Click(object sender, EventArgs e)
+        private void BtnClose_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void FrmPreferences_Load(object sender, EventArgs e)
+        {
+            TxyModsDir.Value = Json.Read(MODS_DIR)!;
         }
     }
 }
