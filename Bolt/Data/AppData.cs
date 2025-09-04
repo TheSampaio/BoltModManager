32c6f4ac8d8996af0ed0c9b9781e0467cedff0ac
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-09-04 11:44:35 -0300

    Add summaries to data and utilities classes

--- Bolt/Data/AppData.cs
+++ Bolt/Data/AppData.cs
@@ -1,8 +1,18 @@
 ﻿namespace Bolt.Data
 {
+    /// <summary>
+    /// Contains application-wide file names for saving game data and preferences.
+    /// </summary>
     internal class AppData
     {
+        /// <summary>
+        /// The default file name used to save and load game data.
+        /// </summary>
         public static string GameFile { get; } = "game.bltg";
+
+        /// <summary>
+        /// The default file name used to save and load user preferences.
+        /// </summary>
         public static string PreferencesFile { get; } = "preferences.blts";
     }
 }
