f5673ba203cb1323737cc1928659cb6aef9264fe
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-09-05 21:26:22 -0300

    Add an Archive class to extract Zip files

---
+++ Bolt/Utilities/Archive.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+
+namespace Bolt.Utilities
+{
+    internal class Archive
+    {
+        public static void Extract(string filePath, string targetPath)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(filePath, targetPath);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Failed to extract archive:\n{ex.Message}",
+                    "Extraction Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+    }
+}
